69f6b619efe92023b56359c677099ef2a58f16cb
Braian Ledesma <braian.ledesma-99@hotmail.com>
2018-10-02 21:41:52 -0300

    func updateCustomer added

--- src/components/CreateCustomer/CreateCustomer.js
+++ src/components/CreateCustomer/CreateCustomer.js
@@ -8,8 +8,30 @@ import '../../css/Create.css';
 class CreateCustomer extends Component {
     state = {
         inputName: '',
-        error: null
+        error: null,
+        titleButton: 'crear',
+        customerId: null,
     };
+
+    componentDidMount() {
+        let customerId = this.props.match.params.customerId
+        if (customerId) {
+            axios.get(`http://localhost:4000/customerById/${customerId}`)
+                .then(response => {
+                    const { data } = response
+                    this.setState({
+                        inputName: data.nombre,
+                        customerId: data.id,
+                        titleButton: 'Actualizar cliente'
+                    })
+                })
+                .catch(err => {
+                    this.setState({
+                        error: err.message
+                    })
+                });
+        }
+    }
 
     getInputName = (event) => {
         let inputName = event.target.value;
@@ -19,17 +41,44 @@ class CreateCustomer extends Component {
     saveCustomer = () => {
         axios.post('http://localhost:4000/customers', {
             name: this.state.inputName
-        }).then(response => {
-            console.log('Its Works')
+        }).then(() => {
+            this.props.history.push("/Customers");
         }).catch(err => {
             this.setState({
                 error: err.message
             })
         });
-        this.props.history.push("/Customers");
+
     };
 
+    updateCustomerAsync = async () => {
+        const { inputName, customerId, error } = this.state
+        const response = await axios.put(`http://localhost:4000/customers/${customerId}`, {
+            name: inputName
+        })
+            .catch(err => {
+                this.setState({
+                    error: err.message
+                })
+            })
+        if (response && !error) {
+            this.props.history.push('/Customers')
+        }
+    }
+
+    buttonHandler = () => {
+        const { customerId } = this.state
+        if (!customerId) {
+            this.saveCustomer()
+        } else {
+            this.updateCustomerAsync()
+        }
+    }
+
     render() {
+        const { titleButton, inputName, error } = this.state
+        const errorM = (<div className="error">{error}</div>)
+
         return (
             <div className="Create-content">
                 <div className="Create-header">
@@ -38,10 +87,15 @@ class CreateCustomer extends Component {
                 <div className="form">
                     <div className="form-group">
                         <label>Nombre:</label>
-                        <input type="text" onChange={(event) => this.getInputName(event)} />
+                        <input type="text" onChange={(event) => this.getInputName(event)} value={inputName} />
                     </div>
                     <div className="form-group">
-                        <button className="button-create" onClick={this.saveCustomer}>Crear</button>
+                        <button className="button-create" onClick={this.buttonHandler}>
+                            { titleButton }
+                        </button>
+                        {
+                            error ? errorM : ''
+                        }
                     </div>
                 </div>
             </div>
